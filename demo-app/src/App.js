db6b2c905b345e279031f6a07202e3481d177c28
Mahnoor Siddiqui <ayaankhan7532@gmail.com>
2021-09-24 22:07:49 +0500

    class 3 p 1

---
+++ demo-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Dinner from './dinner';
+function App() {
+  return (
+    <div>
+       <h1>Hello world</h1>
+       <Dinner className="class 3 part 1"/>
+    </div>
+  )
+}
+
+export default App;
---
+++ demo-app/src/dinner.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+function Dinner(props){
+    return(
+        <div>
+             <h1>Today we are learning react{props.className} </h1>
+        </div>
+    )
+
+}
+
+export default Dinner;
