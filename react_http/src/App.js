fad2504828f79240f3f68701067ac96465990497
Mahnoor Siddiqui <ayaankhan7532@gmail.com>
2021-10-07 16:19:46 +0500

    HTTP GET and POST req

---
+++ react_http/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import PostForm from './components/PostForm';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <PostForm/>
+     {/* <PostList/> */}
+    </div>
+  );
+}
+
+export default App;
